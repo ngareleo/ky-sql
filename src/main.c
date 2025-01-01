e7b3f6f2eb07667b85c17d80a3f0298001764827
ngarimwenda@gmail.com <ngarimwenda@gmail.com>
2025-01-01 22:58:39 +0300

    Add target file checking

--- src/main.c
+++ src/main.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include "./dtypes.h"
 
 #define BUFFER_MAX 200 
+#define EXIT_MESSAGE "\nFarewell traveler!!\n"
 #define QUIT "quit"
+
 
 typedef struct {
     char *target;
 } Config;
 
-int makeConfig(Config *config, int argc, char *argv[]);
-int readShellTurn(char *buffer, int max);
+int make_config(Config *config, struct stat *s, int argc, char *argv[]);
+int read_shell_turn(char *buffer, int max);
+int parse_table_defs(TableDef *def, Config *config);
+int parse_shell_input(char *in);
 
 int main(int argc, char *argv[]) {
     char input[BUFFER_MAX], *pinput = input;
     FILE *tstream;
     Config config, *pconfig = &config;
+    struct stat sb, *psb = &sb;
 
-    if (makeConfig(pconfig, argc, argv) < 0) {
+    if (make_config(pconfig, psb, argc, argv) < 0) {
         printf("Usage for %s:\n", *argv);
         return -1;
     }
@@ -28,18 +36,21 @@ int main(int argc, char *argv[]) {
     }
 
     while (1) {
-        int in = readShellTurn(pinput, BUFFER_MAX);
+        int in = read_shell_turn(pinput, BUFFER_MAX), parse;
 
-        if (in == 0) {
-            printf("Farewell traveler!!\n");
-            break;
-        } else if (in < 0) {
+        if (in < 0) {
             printf("Error reading input. Try again.\n");
             break;
-        } else {
-            // for now we spit back
-            printf("%s\n", pinput);        
         }
+
+        parse = parse_shell_input(pinput);
+        if ( parse < 0) {
+            printf("Trouble parsing input\n");
+            break;
+        } else if (parse == 0) {
+            printf(EXIT_MESSAGE);
+            break;
+        } 
 
         memset(pinput, '\0', in);
     }
@@ -48,20 +59,24 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-int makeConfig(Config *config, int argc, char *argv[]) {
+int make_config(Config *config, struct stat *s, int argc, char *argv[]) {
     if (argc < 2) {
         return -1;
+    }
+
+    int ret = stat(*(argv + 1), s);
+    if (ret < 0) {
+        if (errno == ENOENT) {
+            // we setup a file
+            printf("We setup a file at path %s\n", *(argv + 1));
+            return -1;
+        }
     }
     config->target = *(argv + 1);
     return 0;
 } 
 
-
-/**
- * Reads turn input from the shell. 
- * Returns the number of bytes written per turn, 0 if user quit and returns <0 on error
- */
-int readShellTurn(char *buffer, int max) {
+int read_shell_turn(char *buffer, int max) {
     char c;
     int i = 0;
 
@@ -73,9 +88,27 @@ int readShellTurn(char *buffer, int max) {
         *(buffer + i++) = c;
     }
 
-    if (!strcmp(buffer, QUIT)){
-        return 0;
+    return i;
+}
+
+int parse_table_defs(TableDef *def, Config *config) {
+    FILE *stream;
+    char buf[1000], *pbuf = buf;
+
+    stream = fopen(config->target, "r+");
+
+    size_t nr;
+    if (!(nr = fread(def, sizeof(TableDef), 1, stream))) {
+        perror("fread");
+        return -1;
     }
 
-    return i;
+    return 0;
+}
+
+int parse_shell_input(char *in) {
+    if (!strcmp(in, QUIT)){
+        return 0;
+    }
+    return -1;
 }
